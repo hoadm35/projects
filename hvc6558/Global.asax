5eb506a7d813059d55572f0e1bf6d7fa97f3abfa
hoadm35 <hoadm35@gmail.com>
2017-11-23 10:21:19 +0700

    Add project files.

---
+++ hvc6558/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hvc6558.MvcApplication" Language="C#" %>
---
+++ hvc6558/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hvc6558.Startup))]
+namespace hvc6558
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
